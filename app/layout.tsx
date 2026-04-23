a198c60c9caf973a4676d8d3e014a01fc7a3e4d4
Denis Mikhailov <denis.mikhailov@lottoland.com>
2026-04-23 11:47:24 +0200

    feat: add Google Tag Manager integration and update VS Code git settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NCQN2QSV');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -16,7 +29,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             `,
           }}
         />
-        <script
+        {/* <script
           dangerouslySetInnerHTML={{
             __html: `
               (function(c,l,a,r,i,t,y){
@@ -26,9 +39,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               })(window, document, "clarity", "script", "vy633p1fib");
             `
           }}
-        />
+        /> */}
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NCQN2QSV"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Analytics />
         {children}
       </body>
